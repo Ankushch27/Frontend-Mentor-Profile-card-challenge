8e46f9ab14010c1211325f70617f08490f0395cb
Ankushch27 <ankushch2712@gmail.com>
2021-11-16 13:22:55 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'pattern': 'url(/images/bg-pattern-top.svg), url(/images/bg-pattern-bottom.svg)',
+      },
+      backgroundPosition: {
+        'pattern-pos': 'right 45vw bottom 25vw, left 47vw top 23vw'
+      },
+      colors: {
+        'dark-cyan': 'hsl(185, 75%, 39%)'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
